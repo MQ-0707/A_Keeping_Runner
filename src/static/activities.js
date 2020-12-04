c66262991e5ddb898ad29a80b9cef0a83dea1a6d
MQ-0707 <lwcse35@gmail.com>
2020-12-04 00:26:48 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -3218,6 +3218,20 @@ const activities = [
     "average_heartrate": null,
     "average_speed": 2.3673787394957984,
     "streak": 1
+  },
+  {
+    "run_id": 9223370429947246677,
+    "name": "run from keep",
+    "distance": 1015.37225,
+    "moving_time": "0:09:33",
+    "type": "Run",
+    "start_date": "2020-12-02 11:02:33",
+    "start_date_local": "2020-12-02 19:02:33",
+    "location_country": "{'latitude': 31.401600211217374, 'longitude': 121.35639855777589, 'country': '\u4e2d\u56fd', 'nationCode': '156', 'province': '\u4e0a\u6d77\u5e02', 'city': '\u4e0a\u6d77\u5e02', 'cityCode': '021'}",
+    "summary_polyline": "",
+    "average_heartrate": null,
+    "average_speed": 1.7720283595113437,
+    "streak": 2
   }
 ];
 
